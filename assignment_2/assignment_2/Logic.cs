e5c6b89ae66f7c70f2d242764de96e5fd3025ab4
benas012 <benas.skripkiunas@gmail.com>
2022-10-18 15:02:54 +0300

    created assignment 2

---
+++ assignment_2/assignment_2/Logic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    internal class Logic
+    {
+        public static int CalculateBranchHeight(Branch root)
+        {
+            
+
+            if (root.branches.Count == 0)
+            {
+                return 1;
+            }
+
+            int max = 1;
+
+            foreach (var branch in root.branches)
+            {
+                var currentBranchHeight = CalculateBranchHeight(branch);
+                if (currentBranchHeight > max)
+                {
+                    max = currentBranchHeight;
+                }
+            }
+            return max + 1;
+        }
+    }
+}
---
+++ assignment_2/assignment_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using assignment_2;
+
+
+// 1st Layer
+Branch root = new Branch();
+
+// 2nd Layer
+root.branches.Add(new Branch());
+root.branches.Add(new Branch());
+
+// 3rd Layer
+root.branches[0].branches.Add(new Branch());
+
+root.branches[1].branches.Add(new Branch());
+root.branches[1].branches.Add(new Branch());
+root.branches[1].branches.Add(new Branch());
+
+// 4th Layer
+
+root.branches[1].branches[0].branches.Add(new Branch());
+
+root.branches[1].branches[1].branches.Add(new Branch());
+root.branches[1].branches[1].branches.Add(new Branch());
+
+root.branches[1].branches[2].branches.Add(new Branch());
+
+// 5th Layer
+
+root.branches[1].branches[1].branches[0].branches.Add(new Branch());
+
+
+// 6th Layer
+
+root.branches[1].branches[1].branches[0].branches[0].branches.Add(new Branch());
+
+Console.WriteLine(Logic.CalculateBranchHeight(root));
